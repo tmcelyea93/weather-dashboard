4b79d998378a034693d30e22ed44bbfd8b00113a
tmcelyea93 <tarahmcelyea@gmail.com>
2020-06-28 18:23:47 -0500

    add functions for history

--- assets/logic.js
+++ assets/logic.js
@@ -19,3 +19,41 @@ var uvQry = "https://api.openweathermap.org/data/2.5/uvi?appid="+authKey;
 
 
 /*Functions*/
+function printButtons(){
+    //clear previous buttons
+    searchedCitysGroup.empty();
+
+    for(city of cityHist){
+        searchedCitysGroup.prepend( $("<button/>",{class:"btn btn-secondary btn-white city-btn", 'data-city':city, text:city}) )
+    }
+    // print is called whenever buttons change, so it makes sense to add the save function here too
+    saveHist();
+
+}
+
+function saveHist(){
+    localStorage.setItem('history',JSON.stringify(cityHist));
+}
+
+
+function loadHist(){
+    cityHist = JSON.parse(localStorage.getItem('history'));
+    if(cityHist === null){
+        cityHist = [];
+    }
+}
+
+
+function kelvToF(temp){
+    return ((temp - 273.15)*(9/5) + 32).toFixed(2);
+}
+
+
+function getIcon(weather){  
+    weatherIcon = $("<i/>");
+    //accounting for possibility of multiple weather icons, as described in the api documentation
+    for (const elem of weather){
+        weatherIcon.append( $("<img/>",{src:'http://openweathermap.org/img/wn/'+elem.icon+'@2x.png', alt:elem.description}))
+    }
+    return weatherIcon;
+}
